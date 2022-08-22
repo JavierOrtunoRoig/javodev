947963b62593a7ef247b369c958a56bb632a298c
Javier Ortuño Roig <javi_roig@hotmail.es>
2022-08-22 22:15:42 +0200

    feat: :tada: first commit

---
+++ astro.config.mjs
@@ -0,0 +1,18 @@
+import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless';
+import mdx from '@astrojs/mdx';
+
+import sitemap from '@astrojs/sitemap';
+
+// https://astro.build/config
+export default defineConfig({
+	site: 'https://example.com',
+	integrations: [mdx(), sitemap()],
+	output: 'server',
+	adapter: vercel(),
+	markdown: {
+		shikiConfig: {
+      theme: 'one-dark-pro',
+    }
+	}
+});
---
+++ src/config.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Javier Ortuño Blog';
+export const SITE_DESCRIPTION = 'Este es un blog donde iré subiendo contenido sobre libros, trucos de programación y mucho más!!';
